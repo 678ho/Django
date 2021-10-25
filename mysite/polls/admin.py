49c4cb9f5c1c0d9bc8c7cab8cc8a08ae05646172
Kwang ho Kwak <678hoa@gmail.com>
2021-10-25 16:28:23 +0900

    주석 추가

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,13 +12,12 @@ class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text']
     fieldsets = [
         ('Question Statement', {'fields': ['question_text']}),
-        #('Date Information', {'fields': ['pub_date']}),
         ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date')  #레코드 리스트 컬럼 항목 지정
+    list_filter = ['pub_date'] #필터 사이드바 추가
+    search_fields = ['question_text'] #검색 박스 추가
 
 
 admin.site.register(Question, QuestionAdmin)
