220c825b9d6c9de4f1578d7c7d7f026d54ebdf15
Kwang ho Kwak <678hoa@gmail.com>
2021-10-25 16:51:00 +0900

    django-extensions 추가

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',		# shkim
+    'polls.apps.PollsConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
