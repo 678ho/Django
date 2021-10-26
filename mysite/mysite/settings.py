2fb8c6609864a282fe9c8f7587a2a46dd0bc9916
Kwang ho Kwak <678hoa@gmail.com>
2021-10-26 15:13:45 +0900

    books 추가

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
     'django_extensions',
+    'books.apps.BooksConfig',
+    
 ]
 
 MIDDLEWARE = [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # shkim
     path('polls/', include('polls.urls')),
+    path('books/', include('books.urls')),
 ]
