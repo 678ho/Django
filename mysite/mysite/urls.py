731604c07141041c348929c2e54e1e412f59b21e
Kwang ho Kwak <678hoa@gmail.com>
2021-10-28 13:14:24 +0900

    메인사이트 설정연결

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mysite import views
+from mysite import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.HomeView.as_view(), name='home'),
+
+    path('', view.HomeView.as_view(), name='home'),
 
     path('polls/', include('polls.urls')),
     path('books/', include('books.urls')),
